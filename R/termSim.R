0656e845860d14e054670ffc246a1c53f699299c
guangchuang yu <guangchuangyu@gamil.com>
2018-01-31 17:23:53 +0800

    bug fixed

--- R/termSim.R
+++ R/termSim.R
@@ -16,7 +16,7 @@ termSim <- function(t1,
                     semData,
                     method=c("Wang","Resnik","Rel","Jiang","Lin")
                     ) {
-                    
+
     method <- match.arg(method)
 
     if (all(is.na(t1)) || all(is.na(t2)))
@@ -24,8 +24,8 @@ termSim <- function(t1,
 
     t1 <- t1[!is.na(t1)]
     t2 <- t2[!is.na(t2)]
-    t1 <- unique(t1)
-    t2 <- unique(t2)
+    ## t1 <- unique(t1)
+    ## t2 <- unique(t2)
 
     if ( method %in% c("Resnik", "Jiang", "Lin", "Rel") ) {
         return(infoContentMethod(t1, t2, method=method, semData))
